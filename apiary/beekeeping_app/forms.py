413814547904071b935a7a1d7c5a78b96681f8e8
oliviabach3407 <oliviabach3407@gmail.com>
2024-04-01 16:47:37 -0600

    Majority of Sprint01

---
+++ apiary/beekeeping_app/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Hive
+from .models import Apiary
+
+#all hive forms use this base:
+class HiveForm(forms.ModelForm):
+    class Meta: 
+        model = Hive
+        #by default, ModelForm populates every form with all of the variables in the Project Model
+        #we don't want the keeper being able to choose which portfolio to link to their project, so exclude it
+        exclude = ['apiary'] 
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
+class ApiaryForm(forms.ModelForm):
+    class Meta:
+        model = Apiary
+        fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'contact_email': forms.TextInput(attrs={'class': 'form-control'}),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}), #a checkbox
+            'about': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
--- apiary/beekeeping_app/models.py
+++ apiary/beekeeping_app/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.urls import reverse
+from django.forms import ModelForm
 
 # Create your models here.
+
+#a keeper should have only ONE apiary (one-to-one)
+class Apiary(models.Model):
+
+    title = models.CharField(max_length=200)
+    contact_email = models.CharField(max_length=200)
+    is_published = models.BooleanField(default=False)
+    #might need to change the max length here
+    #optional to fill out, so blank property
+    about = models.CharField(max_length=400, blank = True)
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.title
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('apiary-detail', args=[str(self.id)])
+
+#an Apiary should have MANY hives (one-to-many)
+class Hive(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=400)
+
+    #one apiary, many hives
+    apiary = models.ForeignKey(Apiary, on_delete=models.CASCADE)
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.title
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('hive-detail', args=[str(self.id)])
+    
+class HiveForm(ModelForm):
+    class Meta:
+        model = Hive
+        fields = ["title", "description"]
+
+class Keeper(models.Model):
+
+    name = models.CharField(max_length=200)
+    email = models.CharField("Email", max_length=200)
+    #removed blank = true
+
+    #one keeper, one apiary
+    apiary = models.OneToOneField(Apiary, on_delete=models.CASCADE)
+
+    #Define default String to return the name for representing the Model object."
+    def __str__(self):
+        return self.name
+
+    #Returns the URL to access a particular instance of MyModelName.
+    #if you define this method then Django will automatically
+    # add a "View on Site" button to the model's record editing screens in the Admin site
+    def get_absolute_url(self):
+        return reverse('keeper-detail', args=[str(self.id)])
